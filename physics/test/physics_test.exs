827986280c0023aab29fa68efbe378b7ab25a3d1
Charles FD <cfreiden@yahoo.fr>
2016-03-29 08:42:22 +0200

    Exercice until chapter 5

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -5,4 +5,12 @@ defmodule PhysicsTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "convert_to_km" do
+    assert Calcs.convert_to_km(1_000) == 1
+  end
+
+  test "rounded_to_nearest_tenth" do
+    assert Calcs.rounded_to_nearest_tenth(3.14) == 3.2
+  end
 end
